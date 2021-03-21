5d065c7b74e36ece35efc647bda2932cad0b700d
HaoPeng <haopeng__@outlook.com>
2021-03-21 19:39:12 +0800

    can't run in release mode

---
+++ TestGrpc/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using Grpc.Core;
+using Helloworld;
+
+namespace TestGrpc
+{
+    public class Class1
+    {
+        public static void A()
+        {
+            Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new Greeter.GreeterClient(channel);
+            String user = "you";
+
+            try
+            {
+                //var reply = client.SayHello(new HelloRequest { Name = user });
+
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"{e.Message}");
+            }
+        }
+    }
+}
--- UWPTest/MainPage.xaml.cs
+++ UWPTest/MainPage.xaml.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using TestGrpc;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,28 +31,11 @@ namespace UWPTest
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private ObservableCollection<string> _source;
         public MainPage()
         {
             this.InitializeComponent();
-
-            _source = new ObservableCollection<string>(Enumerable.Range(0, 20).Select(x=>x.ToString()));
-            ListView1.ItemsSource = _source;
-
-            Debug.WriteLine($"{ListView1.Items.GetHashCode()}");
-            ListView1.Items.VectorChanged += Items_VectorChanged;
-            Debug.WriteLine($"{ListView1.Items.GetHashCode()}");
+            Class1.A();
         }
 
-        private void Items_VectorChanged(IObservableVector<object> sender, IVectorChangedEventArgs @event)
-        {
-            Debug.WriteLine($"{sender.GetHashCode()}");
-            Debug.WriteLine($"{ListView1.Items.GetHashCode()}");
-        }
-
-        private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
-        {
-            _source.Remove((sender as FrameworkElement).DataContext as string);
-        }
     }
 }
